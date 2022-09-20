9d4733fb66b8a2df7f7704dbd182cfceba64d043
RafaAlbano <rafaelaalbano70gmail.com>
2022-09-20 09:26:55 -0300

    200922

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      cep: "",
+      endereco: {},
+    };
+  },
+  methods: {
+    async buscar() {
+      const url = `https://viacep.com.br/ws/${this.cep}/json/`;
+      const { data } = await axios.get(url);
+      this.endereco = data;
+    },
+  },
+};
+</script>
+<template>
+  <h1>Buscar Cep</h1>
+  <form @submit.prevent="buscar">
+    <input type="text" v-model="cep" />
+    <button type="submit">Buscar</button>
+  </form>
+  <form>
+    <label for="rua">Rua</label>
+    <input name="rua" type="text" v-model="endereco.logradouro" />
+    <label for="numero">Numero</label>
+    <input name="numero" type="text" v-model="endereco.numero" />
+    <label for="localidade">Cidade</label>
+    <input for="localidade" type="text" v-model="endereco.localidade" />
+    <label for="complemento">Complemento</label>
+    <input name="complemento" type="text" v-model="endereco.logradouro" />
+  </form>
+  {{ endereco }}
+</template>
+<style></style>
